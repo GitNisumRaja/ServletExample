cccc4529789ff9f6bb3ac54fbce3a0d3add0c3e4
Afroz <nisum@192.168.1.9>
2019-07-18 06:40:20 +0530

    simple HttpSession Demo

---
+++ Serv04_HttpSessionDemo/src/com/insan/session/practice/SecondServlet.java
@@ -0,0 +1,23 @@
+package com.insan.session.practice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SecondServlet extends HttpServlet {
+	
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+	 	out.println("");
+	 	HttpSession session = req.getSession(false);
+	 	out.println("HttpSession value Read::"+session.getAttribute("name"));
+	}
+
+}
