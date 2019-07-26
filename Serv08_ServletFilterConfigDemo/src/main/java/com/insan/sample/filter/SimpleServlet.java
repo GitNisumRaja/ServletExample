dbc541cbe8144ef36d8f8b24054942d0b3a4d405
Afroz <nisum@192.168.1.9>
2019-07-26 19:04:05 +0530

    SERV-108::FilterConfig demo, we created only SimpleServlet class

---
+++ Serv08_ServletFilterConfigDemo/src/main/java/com/insan/sample/filter/SimpleServlet.java
@@ -0,0 +1,24 @@
+package com.insan.sample.filter;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/serv")
+public class SimpleServlet extends HttpServlet {
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.println("Welcome to Servlet page");
+	}
+
+}
